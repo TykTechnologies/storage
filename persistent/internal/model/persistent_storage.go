29fafddc3af8adbba8c223a1ed07d46e3a6865cf
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-10 17:21:35 -0300

    avoid cycling

--- persistent/internal/model/persistent_storage.go
+++ persistent/internal/model/persistent_storage.go
@@ -2,9 +2,9 @@ package model
 
 import (
 	"context"
-	"github.com/TykTechnologies/storage/persistent"
+	"github.com/TykTechnologies/storage/persistent/id"
 )
 
 type PersistentStorage interface {
-	Insert(context.Context, DBTable, persistent.DBObject) error
+	Insert(context.Context, DBTable, id.DBObject) error
 }
