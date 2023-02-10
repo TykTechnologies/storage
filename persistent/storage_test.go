4a71b6bd93edc4b281a242a56af205c63009eef4
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-10 10:40:22 -0300

    update TestNewPersistentStorage test

--- persistent/storage_test.go
+++ persistent/storage_test.go
@@ -3,7 +3,6 @@ package persistent
 import (
 	"testing"
 
-	"github.com/TykTechnologies/storage/persistent/internal/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,10 +11,15 @@ func TestNewPersistentStorage(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc, func(t *testing.T) {
-			_, err := NewPersistentStorage(&model.ClientOpts{Type: tc})
+			_, err := NewPersistentStorage(&ClientOpts{Type: tc})
 
 			// at this moment we expect an error until the driver is implemented
-			assert.Error(t, err)
+
+			if tc == Mgo {
+				assert.Nil(t, err)
+			} else {
+				assert.Error(t, err)
+			}
 		})
 	}
 }
