8f1cf085a1ee836078ca7d0a6cb429e5f54b3558
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-14 16:20:35 -0300

    reimlement bson internal type

--- persistent/internal/driver/mgo/bson.go
+++ persistent/internal/driver/mgo/bson.go
@@ -8,10 +8,42 @@ import (
 
 // inherit all the bson methods from mgo.v2. Implements the
 // `mode.BSON` interface
-type mgoBson struct {
-	bson.ObjectId
+type mgoBson bson.ObjectId
+
+func (m *mgoBson) Hex() string {
+	return bson.ObjectId(*m).Hex()
+}
+
+func (m *mgoBson) String() string {
+	return m.Hex()
+}
+
+func (m *mgoBson) Valid() bool {
+	return bson.ObjectId(*m).Valid()
+}
+
+func (m *mgoBson) MarshalJSON() ([]byte, error) {
+	return bson.ObjectId(*m).MarshalJSON()
+}
+
+func (m *mgoBson) UnmarshalJSON(bytes []byte) error {
+	var b bson.ObjectId
+	b.UnmarshalJSON(bytes)
+	*m = mgoBson(string(b))
+
+	return nil
+}
+
+func (m *mgoBson) MarshalText() ([]byte, error) {
+	bsonId := bson.ObjectId(*m)
+	return bsonId.MarshalText()
+}
+
+func (m *mgoBson) UnmarshalText(bytes []byte) error {
+	bsonId := bson.ObjectId(*m)
+	return bsonId.UnmarshalText(bytes)
 }
 
 func (m *mgoBson) Timestamp() time.Time {
-	return m.Time()
+	return bson.ObjectId(*m).Time()
 }
