50ef47fca453274c95432c6f62b77ebe8be04902
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-15 11:57:00 -0300

    added more tests

--- persistent/internal/driver/mgo/bson_test.go
+++ persistent/internal/driver/mgo/bson_test.go
@@ -20,4 +20,27 @@ func TestNewObjectID(t *testing.T) {
 	assert.Equal(t, objectToCompare.Hex(), id.Hex())
 	assert.Equal(t, objectToCompare.Time(), id.Timestamp())
 	assert.Equal(t, objectToCompare.Valid(), id.Valid())
+
+	//Marshall text test
+	t.Run("Marshal Text", func(t *testing.T) {
+		otcBytes, err := objectToCompare.MarshalText()
+		idBytes, err2 := id.MarshalText()
+
+		if err != nill || err2 != nil {
+			t.Error(err.Error())
+		}
+
+		assert.Equal(t, otcBytes, idBytes)
+	})
+
+	t.Run("Marshal JSON", func(t *testing.T) {
+		otcBytes, err := objectToCompare.MarshalJSON()
+		idBytes, err2 := id.MarshalJSON()
+
+		if err != nill || err2 != nil {
+			t.Error(err.Error())
+		}
+
+		assert.Equal(t, otcBytes, idBytes)
+	})
 }
