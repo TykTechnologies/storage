44e6a6b760621504208085a58fb05606a5ddbb7a
tbuchaillot <tombuchaillot89@gmail.com>
2023-02-17 16:38:58 +0100

    simplifying bson handling

--- persistent/internal/model/persistent_storage.go
+++ persistent/internal/model/persistent_storage.go
@@ -8,13 +8,9 @@ import (
 
 type PersistentStorage interface {
 	// Insert a DbObject into the database
-	Insert(context.Context, DBTable, id.DBObject) error
-	// NewObjectID returns a new id object
-	NewObjectID() id.ObjectID
-	// ObjectIdHex returns an object id created from an existent id
-	ObjectIdHex(id string) id.ObjectID
+	Insert(context.Context, id.DBObject) error
 	// Delete a DbObject from the database
-	Delete(ctx context.Context, table DBTable, row id.DBObject) error
-	// Checking if an error is a "no rows error"
+	Delete(context.Context, id.DBObject) error
+	// IsErrNoRows Checking if an error is a "no rows error"
 	IsErrNoRows(err error) bool
 }
