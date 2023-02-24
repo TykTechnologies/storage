b03828daf3ab95877ab1bde673bc1cbf035ecfb7
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-24 20:20:44 -0300

    implemented missing methods for id type, renamed from OID to ObjectId

--- persistent/id/object_id.go
+++ persistent/id/object_id.go
@@ -1,21 +1,65 @@
 package id
 
-import "time"
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
 
-type OID string
+type ObjectId string
+
+func NewObjectID() ObjectId {
+	return ObjectId(bson.NewObjectId())
+}
 
 // Valid returns true if id is valid. A valid id must contain exactly 12 bytes.
-func (id OID) Valid() bool {
+func (id ObjectId) Valid() bool {
 	return len(id) == 12
 }
 
-type ObjectID interface {
-	Hex() string
-	String() string
-	Timestamp() time.Time
-	Valid() bool
-	MarshalJSON() ([]byte, error)
-	UnmarshalJSON([]byte) error
-	MarshalText() ([]byte, error)
-	UnmarshalText([]byte) error
+func (id ObjectId) Hex() string {
+	return hex.EncodeToString([]byte(id))
+}
+
+func (id ObjectId) String() string {
+	return fmt.Sprintf("ObjectID(%q)", id.Hex())
+}
+
+func (id ObjectId) Timestamp() time.Time {
+	bytes := []byte(string(id)[0:4])
+	secs := int64(binary.BigEndian.Uint32(bytes))
+	return time.Unix(secs, 0)
+}
+
+func (id ObjectId) MarshalJSON() ([]byte, error) {
+	return json.Marshal(id.Hex())
+}
+
+func (id *ObjectId) UnmarshalJSON(buf []byte) error {
+	var b bson.ObjectId
+	b.UnmarshalJSON(buf)
+	*id = ObjectId(string(b))
+
+	return nil
+}
+
+// ObjectIdHex useful to create an object ID from the string
+func ObjectIdHex(id string) ObjectId {
+	return ObjectId(bson.ObjectIdHex(id))
+}
+
+func IsObjectIdHex(s string) bool {
+	if len(s) != 24 {
+		return false
+	}
+	_, err := hex.DecodeString(s)
+	return err == nil
+}
+
+// GetBSON only used by mgo
+func (id ObjectId) GetBSON() (interface{}, error) {
+	return bson.ObjectId(id), nil
 }
--- persistent/id/object_id_test.go
+++ persistent/id/object_id_test.go
@@ -2,6 +2,8 @@ package id
 
 import (
 	"encoding/hex"
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,17 +13,17 @@ func TestValidObjectId(t *testing.T) {
 	valid, _ := hex.DecodeString("63efa41f4713944d6f696768")
 	tcs := []struct {
 		testName      string
-		givenObjectId OID
+		givenObjectId ObjectId
 		expectedValid bool
 	}{
 		{
 			testName:      "valid",
-			givenObjectId: OID(valid),
+			givenObjectId: ObjectId(valid),
 			expectedValid: true,
 		},
 		{
 			testName:      "invalid",
-			givenObjectId: OID("test"),
+			givenObjectId: ObjectId("test"),
 			expectedValid: false,
 		},
 	}
@@ -33,4 +35,62 @@ func TestValidObjectId(t *testing.T) {
 			assert.Equal(t, tc.expectedValid, actual)
 		})
 	}
+}
+
+func TestNewObjectID(t *testing.T) {
+	id := NewObjectID()
+	assert.Equal(t, 12, len(id))
+}
+
+func TestHex(t *testing.T) {
+	id := NewObjectID()
+	expected := hex.EncodeToString([]byte(id))
+	assert.Equal(t, expected, id.Hex())
+}
+
+func TestString(t *testing.T) {
+	id := NewObjectID()
+	bsonID := bson.ObjectId(id)
+	assert.Equal(t, fmt.Sprintf("ObjectID(%q)", bsonID.Hex()), id.String())
+}
+
+func TestTimestamp(t *testing.T) {
+	id := NewObjectID()
+	bsonID := bson.ObjectId(id)
+
+	assert.Equal(t, bsonID.Time(), id.Timestamp())
+}
+
+func TestMarshalJSON(t *testing.T) {
+	id := NewObjectID()
+	bsonID := bson.ObjectId(id)
+
+	bsonBytes, err1 := bsonID.MarshalJSON()
+	idBytes, err2 := id.MarshalJSON()
+
+	if err1 != nil || err2 != nil {
+		t.Fatal("failed marshaling object id")
+	}
+	assert.Equal(t, bsonBytes, idBytes)
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	id := NewObjectID()
+	idBytes, err := id.MarshalJSON()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var id2 ObjectId
+	id2.UnmarshalJSON(idBytes)
+
+	assert.Equal(t, id, id2)
+}
+
+func TestIsObjectIdHex(t *testing.T) {
+	id := NewObjectID()
+
+	assert.Equal(t, true, IsObjectIdHex(id.Hex()))
+	assert.Equal(t, false, IsObjectIdHex("any-invalid-value"))
 }
