93224643a75b78b9105e060580ed290cd8d8ca82
sredny buitrago <sredny.buitrago@gmail.com>
2023-02-27 18:06:06 -0300

    expose the official mongo driver

--- persistent/storage.go
+++ persistent/storage.go
@@ -2,6 +2,7 @@ package persistent
 
 import (
 	"errors"
+	"github.com/TykTechnologies/storage/persistent/internal/driver/mongo"
 
 	"github.com/TykTechnologies/storage/persistent/internal/driver/mgo"
 
@@ -23,10 +24,9 @@ func NewPersistentStorage(opts *ClientOpts) (model.PersistentStorage, error) {
 	clientOpts := model.ClientOpts(*opts)
 	switch opts.Type {
 	case OfficialMongo:
-		return nil, errors.New("not implemented")
+		return mongo.NewMongoDriver(&clientOpts)
 	case Mgo:
-		driver, err := mgo.NewMgoDriver(&clientOpts)
-		return driver, err
+		return mgo.NewMgoDriver(&clientOpts)
 	default:
 		return nil, errors.New("invalid driver")
 	}
