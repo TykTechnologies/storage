0d33d8ab6d1c9d69679eb4b25fdd875636c78ce4
Matias <83959431+mativm02@users.noreply.github.com>
2023-11-02 11:27:00 -0300

    improving naming

--- bin/temporal-storage-tests.sh
+++ bin/temporal-storage-tests.sh
@@ -5,18 +5,18 @@ set -e
 cd $(dirname $(dirname $(readlink -f $0)))
 
 # load params
-storagetype=${1}
-db=${2}
+database=${1}
+version=${2}
 
 # Set up connection or other environment variables specific to the Redis test
-if [[ $db = "redis" ]]; then
+if [[ $database = "redis" ]]; then
     export REDIS_CONNECTION_STRING="localhost:6379"
 else
-    echo "unsupported database: $db" >&2
+    echo "unsupported database: $database" >&2
     exit 1
 fi
 
-echo "Running tests with $storagetype storage using $db database:"
+echo "Running tests with using $database version: $version :"
 echo
 
 # Since you mentioned your Redis tests are in the "temporal" folder
@@ -29,11 +29,11 @@ echo
 
 for pkg in $(listPackages);
 do
-    coveragefile=`echo "$pkg-$db" | awk -F/ '{print $NF}'`
+    coveragefile=`echo "$pkg-$database" | awk -F/ '{print $NF}'`
 
     tags=""
     if [[ ${pkg} == *"driver"* ]]; then
-            tags="-tags $db"
+            tags="-tags $database"
     fi
 
     set -x
