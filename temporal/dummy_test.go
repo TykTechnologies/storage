a86e7fbc8818e95531350dacfc037f977b1201a6
Matias <83959431+mativm02@users.noreply.github.com>
2023-11-02 11:07:42 -0300

    testing redis CI with dummy test

---
+++ temporal/dummy_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/go-redis/redis/v8"
+	"golang.org/x/net/context"
+)
+
+func TestRedisPing(t *testing.T) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0, // default db
+	})
+
+	pong, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		t.Fatalf("Couldn't connect to Redis: %v", err)
+	}
+	if pong != "PONG" {
+		t.Fatalf("Redis didn't respond with PONG when Pinged. Got: %v", pong)
+	}
+}
