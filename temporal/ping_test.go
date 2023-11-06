fca81437e6706f773c4c248659f315a9bbbe4f6d
Matias <83959431+mativm02@users.noreply.github.com>
2023-11-06 11:55:13 -0300

    increasing test coverage

--- temporal/ping_test.go
+++ temporal/ping_test.go
@@ -1,6 +1,7 @@
 package temporal
 
 import (
+	"os"
 	"testing"
 )
 
@@ -8,5 +9,11 @@ func TestPing(t *testing.T) {
 	err := Ping()
 	if err != nil {
 		t.Fatalf("Ping function returned an error: %v", err)
+	}
+
+	os.Unsetenv("REDIS_ADDR")
+	err = Ping()
+	if err == nil {
+		t.Fatalf("Ping function should have returned an error when REDIS_ADDR is not set")
 	}
 }
