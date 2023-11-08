199f3a4bba21e5cb8cf3032f2c72b29e08e11954
Matias <83959431+mativm02@users.noreply.github.com>
2023-11-08 14:49:51 -0300

    adding tests for getRedisAddrs

--- temporal/internal/driver/redis8/redis8_test.go
+++ temporal/internal/driver/redis8/redis8_test.go
@@ -97,3 +97,91 @@ func TestNewRedis8(t *testing.T) {
 		})
 	}
 }
+
+func TestGetRedisAddrs(t *testing.T) {
+	tests := []struct {
+		name string
+		opts types.RedisOptions
+		want []string
+	}{
+		{
+			name: "With Addrs",
+			opts: types.RedisOptions{
+				Addrs: []string{"127.0.0.1:6379", "127.0.0.2:6379"},
+			},
+			want: []string{"127.0.0.1:6379", "127.0.0.2:6379"},
+		},
+		{
+			name: "With Hosts map",
+			opts: types.RedisOptions{
+				Hosts: map[string]string{"127.0.0.1": "6379", "127.0.0.2": "6380"},
+			},
+			want: []string{"127.0.0.1:6379", "127.0.0.2:6380"},
+		},
+		{
+			name: "With Host and Port",
+			opts: types.RedisOptions{
+				Host: "127.0.0.1",
+				Port: 6379,
+			},
+			want: []string{"127.0.0.1:6379"},
+		},
+		{
+			name: "With empty options",
+			opts: types.RedisOptions{},
+			want: []string{},
+		},
+		{
+			name: "With Addrs and Hosts map",
+			opts: types.RedisOptions{
+				Addrs: []string{"127.0.0.1:6379"},
+				Hosts: map[string]string{"127.0.0.2": "6380"},
+			},
+			want: []string{"127.0.0.1:6379"}, // Addrs takes priority over Hosts map
+		},
+		{
+			name: "With Addrs and Host/Port",
+			opts: types.RedisOptions{
+				Addrs: []string{"127.0.0.1:6379"},
+				Host:  "127.0.0.2",
+				Port:  6380,
+			},
+			want: []string{"127.0.0.1:6379"}, // Addrs takes priority over Host/Port
+		},
+		{
+			name: "With Hosts map and Host/Port",
+			opts: types.RedisOptions{
+				Hosts: map[string]string{"127.0.0.1": "6379"},
+				Host:  "127.0.0.2",
+				Port:  6380,
+			},
+			want: []string{"127.0.0.1:6379"}, // Hosts map takes priority over Host/Port
+		},
+		{
+			name: "With all empty values",
+			opts: types.RedisOptions{
+				Hosts: map[string]string{},
+				Host:  "",
+				Port:  0,
+			},
+			want: []string{},
+		},
+		{
+			name: "With Port only",
+			opts: types.RedisOptions{
+				Port: 6379,
+			},
+			want: []string{":6379"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getRedisAddrs(&tt.opts)
+			equals := compareUnorderedSlices(t, got, tt.want)
+			if !equals {
+				t.Errorf("getRedisAddrs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
