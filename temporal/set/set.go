6d3517a09757a74a841d638046b135d3ce90b105
Tomas Buchaillot <tombuchaillot89@gmail.com>
2023-11-23 16:05:06 +0100

    TT-10389 Temporal Set interface (#90)
    * set interface
    
    * validation in remove

---
+++ temporal/set/set.go
@@ -0,0 +1,19 @@
+package set
+
+import (
+	"github.com/TykTechnologies/storage/temporal/internal/driver/redisv8"
+	"github.com/TykTechnologies/storage/temporal/model"
+)
+
+type Set = model.Set
+
+var _ Set = (*redisv8.RedisV8)(nil)
+
+func NewSet(conn model.Connector) (Set, error) {
+	switch conn.Type() {
+	case model.RedisV8Type:
+		return redisv8.NewRedisV8WithConnection(conn)
+	default:
+		return nil, model.ErrInvalidHandlerType
+	}
+}
