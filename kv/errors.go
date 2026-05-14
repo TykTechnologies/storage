734aa457acb7110b4b57cc75daba8120736f989e
Vlad Zabolotnyi <vlad.z@tyk.io>
2026-05-14 10:21:22 +0300

    feat: update the errors with adding custom types for key not found and store unavailable that would be used by providers

--- kv/errors.go
+++ kv/errors.go
@@ -6,9 +6,6 @@ import (
 )
 
 var (
-	// ErrKeyNotFound is returned when a requested key doesn't exist in the store.
-	ErrKeyNotFound = errors.New("key not found")
-
 	// ErrStoreNotFound is returned when referencing an unregistered store name.
 	ErrStoreNotFound = errors.New("store not found")
 
@@ -25,10 +22,6 @@ var (
 	ErrStoreRequired = errors.New("required store failed to initialize")
 )
 
-func NewKeyNotFoundError(store, key string) error {
-	return fmt.Errorf("store %q key %q: %w", store, key, ErrKeyNotFound)
-}
-
 func NewStoreNotFoundError(storeName string) error {
 	return fmt.Errorf("store %q: %w", storeName, ErrStoreNotFound)
 }
@@ -43,4 +36,29 @@ func NewFieldNotFoundError(field, jsonValue string) error {
 
 func NewInvalidReferenceError(reference string) error {
 	return fmt.Errorf("reference %q: %w", reference, ErrInvalidReference)
+}
+
+// KeyNotFoundError indicates the store is reachable but the key does not exist.
+type KeyNotFoundError struct {
+	StoreName string
+	KeyPath   string
+}
+
+func (e *KeyNotFoundError) Error() string {
+	return fmt.Sprintf("key %q not found in store %q", e.KeyPath, e.StoreName)
+}
+
+// StoreUnavailableError indicates a transient failure reaching the store.
+type StoreUnavailableError struct {
+	StoreName string
+	KeyPath   string
+	Err       error
+}
+
+func (e *StoreUnavailableError) Error() string {
+	return fmt.Sprintf("store %q unavailable when fetching key %q: %v", e.StoreName, e.KeyPath, e.Err)
+}
+
+func (e *StoreUnavailableError) Unwrap() error {
+	return e.Err
 }
