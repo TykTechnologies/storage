5ef6f9307a1f6974941ab769a1021c68305146bd
Vlad Zabolotnyi <vlad.z@tyk.io>
2026-05-14 18:30:22 +0300

    feat: add secret store implementation

--- kv/store.go
+++ kv/store.go
@@ -3,6 +3,9 @@ package kv
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"golang.org/x/sync/singleflight"
 )
 
 // SecretStore is the high-level interface. It wraps Provider
@@ -12,19 +15,76 @@ type SecretStore interface {
 	GetSecret(ctx context.Context, path string) (string, error)
 }
 
-// TODO: Add single flight field with implementation
 type secretStore struct {
 	name     string
 	provider Provider
 	cache    *cache
+	sf       *singleflight.Group
 }
 
-func NewSecretStore(name string, provider Provider, cacheConfig CacheConfig) (SecretStore, error) {
-	if provider == nil {
-		return nil, fmt.Errorf("failed to create a secret store with name '%s': provider cannot be nil", name)
+// GetSecret retrieves a secret value with caching and deduplication.
+func (s *secretStore) GetSecret(ctx context.Context, path string) (string, error) {
+	val, exists, needsRefresh, err := s.cache.Get(path)
+	if exists {
+		// If value is almost expired on cache, the process should refresh it
+		// on background which is called "stale-while-revalidate" strategy
+		if needsRefresh {
+			s.triggerBackgroundRefresh(path)
+		}
+
+		return val, err
 	}
 
-	cache, err := newCache(cacheConfig)
+	res, fetchErr, _ := s.sf.Do(path, func() (any, error) {
+		fetchCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		newVal, err := s.provider.Get(fetchCtx, path)
+		s.cache.Set(path, newVal, err)
+
+		return newVal, err
+	})
+
+	if fetchErr != nil {
+		return "", fetchErr
+	}
+
+	// Providers always return string but anyway its better to be safe
+	resStr, ok := res.(string)
+	if !ok {
+		// TODO: Replace with some generic fetch error. If the result is not a string,
+		// its a programming mistake as every provider should return strign
+		return "", fmt.Errorf("provider returned unexpected type %T; expected string", res)
+	}
+
+	return resStr, nil
+}
+
+func (s *secretStore) triggerBackgroundRefresh(path string) {
+	go func() {
+		bgCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		_, _, _ = s.sf.Do(path, func() (any, error) {
+			newVal, err := s.provider.Get(bgCtx, path)
+			s.cache.Set(path, newVal, err)
+
+			return newVal, err
+		})
+	}()
+}
+
+func NewSecretStore(
+	ctx context.Context,
+	name string,
+	provider Provider,
+	cacheConfig CacheConfig,
+) (SecretStore, error) {
+	if provider == nil {
+		return nil, fmt.Errorf("failed to create a secret store with name %q: provider cannot be nil", name)
+	}
+
+	cache, err := newCache(ctx, cacheConfig)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create secret store: %w", err)
 	}
@@ -33,10 +93,6 @@ func NewSecretStore(name string, provider Provider, cacheConfig CacheConfig) (Se
 		name:     name,
 		provider: provider,
 		cache:    cache,
+		sf:       &singleflight.Group{},
 	}, nil
-}
-
-// GetSecret retrieves a secret value with caching and deduplication.
-func (s *secretStore) GetSecret(ctx context.Context, path string) (string, error) {
-	return "", nil
 }
