c3b4074a60f66a1a9b78dd6d8e19edf3c403945b
Vlad Zabolotnyi <vlad.z@tyk.io>
2026-07-30 18:22:57 +0300

    feat: remove cache and tests that touched the logic

--- kv/provider.go
+++ kv/provider.go
@@ -119,7 +119,7 @@ type Closer interface {
 }
 
 // Standaloner is an optional interface for providers that do not need
-// to be combined with caching or singleflight mechanisms.
+// to be combined with singleflight mechanisms.
 type Standaloner interface {
 	IsStandalone() bool
 }
